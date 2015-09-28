7b93223f54a4626dd1081c7da06d92d0416dfc0f
Lisa Folliard & Alexa Zeazas <student@epicodus.com>
2015-09-28 10:25:41 -0700

    add a new category

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,12 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    save3(params) {
+      var newCategory = this.store.createRecord('category', params);
+      newCategory.save();
+      this.transitionTo('index');
+    },
+    
     destroyCategory(category) {
       category.destroyRecord();
       this.transitionTo('index');
