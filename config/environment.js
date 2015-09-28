02ca183f4be3850a5ba16c7fe0b2d532ddbf3b14
Lisa Folliard & Alexa Zeazas <student@epicodus.com>
2015-09-28 09:54:11 -0700

    Create category model.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'craigslist-clone',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://craigslist-clone22.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
